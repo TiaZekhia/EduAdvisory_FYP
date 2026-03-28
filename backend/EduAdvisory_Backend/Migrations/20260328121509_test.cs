100dd8268c1db66091d43d07455dcf4cc039c0f9
Georges <georgeselhajj46@gmail.com>
2026-03-28 14:16:02 +0200

    migration

---
+++ backend/EduAdvisory_Backend/Migrations/20260328121509_test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduAdvisory_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "google_access_token",
+                table: "advisor");
+
+            migrationBuilder.DropColumn(
+                name: "google_email",
+                table: "advisor");
+
+            migrationBuilder.DropColumn(
+                name: "google_refresh_token",
+                table: "advisor");
+
+            migrationBuilder.DropColumn(
+                name: "google_token_expiry_utc",
+                table: "advisor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "google_access_token",
+                table: "advisor",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "google_email",
+                table: "advisor",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "google_refresh_token",
+                table: "advisor",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "google_token_expiry_utc",
+                table: "advisor",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
